3957ed2ec5851d1c810abab1aab06e2e659bf037
gthChavandb <dbcIMCpune@gmail.com>
2025-01-17 16:17:18 +0530

    T01HelloWorld first commit

---
+++ T01HelloWorld/src/udmjava/dbcT01miscpkg/HelloWorld2.java
@@ -0,0 +1,15 @@
+package udmjava.dbcT01miscpkg;
+
+public class HelloWorld2 {
+
+	public void showMessage() {
+		System.out.println("Hello World2 dbc");
+	}
+
+	public static void main(String[] args) {
+
+		HelloWorld2 oHelloWorld2= new HelloWorld2();
+		oHelloWorld2.showMessage();
+	}
+
+}
